4a1769f37ffa19b969b5739e2dea802220bb14f0
JBH <xz7840@dgu.edu>
2023-12-19 19:46:22 +0900

    fix: 구글 로그인 urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,8 +6,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('google/login', views.google_login, name='google_login'),
-    # path('google/callback/', views.google_callback, name='google_callback'),  
-    path('google/callback/', views.GoogleAccessView.as_view(), name='googel_access'),
+    path('google/callback/', views.google_callback, name='google_callback'),  
+    # path('google/callback/', views.GoogleAccessView.as_view(), name='googel_access'),
     path('google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
     path('bank', UserBankViewSet.as_view(), name='bank'),
     path('mypage', UserViewSet.as_view(), name='mypage'),
--- charity/models.py
+++ charity/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Charity(models.Model):
-    charity_id = models.IntegerField(primary_key=True)
+    id = models.IntegerField(primary_key=True)
     # fishbread_id = models.ForeignKey(Fishbread, blank=False, null=False, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     content = models.TextField()
